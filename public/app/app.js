61c494672282f260a6610c4e827229c89c5450ad
samofrench <sam.o.french@gmail.com>
2015-12-10 16:04:14 -0800

    Angular stuff

---
+++ public/app/app.js
@@ -0,0 +1,42 @@
+var app = angular.module('GardenApp', ['ngRoute', 'GardenCtrls', 'GardenServices']);
+
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+
+  $routeProvider
+  .when('/', {
+    templateUrl: 'app/views/index.html',
+    controller: 'HomeCtrl'
+  }).when('/gardens', {
+    templateUrl: 'app/views/gardensAll.html',
+    controller: 'GardenCtrl'
+  })
+  .when('/gardens/new', {
+    templateUrl: 'app/views/gardenNew.html',
+    controller: 'NewCtrl'
+  })
+  .when('/gardens/:id', {
+    templateUrl: 'app/views/gardenShow.html',
+    controller: 'ShowCtrl'
+  })
+  .when('/login', {
+    templateUrl: '/app/views/login.html',
+    controller: 'LoginCtrl'
+  })
+  .when('/signup', {
+    templateUrl: '/app/views/login.html',
+    controller: 'SignupCtrl'
+  })
+  .otherwise({
+    templateUrl: 'app/views/404.html'
+  });
+
+  $locationProvider.html5Mode(true);
+}])
+.config(['$httpProvider', function($httpProvider) {
+  $httpProvider.interceptors.push('AuthInterceptor');
+}])
+.run(['$rootScope', 'Auth', function ($rootScope, Auth) {
+  $rootScope.isLoggedIn = function () {
+    return Auth.isLoggedIn.apply(Auth);
+  };
+}]);
