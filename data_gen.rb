806262d871c4edb82ac4453ca806e2559350b3fe
samofrench <sam.o.french@gmail.com>
2015-12-16 14:52:48 -0800

    making data

--- data_gen.rb
+++ data_gen.rb
@@ -1,16 +1,25 @@
+require 'date'
+
 def make_data
 
-	t = Time.now
-	c = 0.5
+	t = Time.new(2015, 12, 14)
+	c = 0.75
 	prev = rand(1..100)
 
-	for i in (1..72)
+	for i in (0..11)
 
 		value = (c*prev + (1 - c)*rand(1..100)).round(0)
-		puts '{date: "' + (t+60*60*i).to_s + '", value: ' + value.to_s + '},'
+		puts '{time: "' + (t + i*60*60*2).to_s + '", value: ' + value.to_s + '},'
 		prev = value
 	end
 
 end
 
+puts "sun"
+make_data
+puts "water"
+make_data
+puts "soil"
+make_data
+puts "temp"
 make_data
---
+++ public/app/66f4-dec15.js
@@ -0,0 +1,13 @@
+{
+    user_id: '66f44430d5d70a81cdafa6c',
+    date: '2015-12-15',
+    data: {
+        "sun": [],
+
+        "water": [],
+
+        "soil": [],
+
+        "temp": []
+    }            
+}
