7d942bab7f89e96a5646fb1c33a1e9363122af3b
alex-mac <meet.alexmac@gmail.com>
2015-12-16 14:52:21 -0800

    created sample data

--- data_gen.rb
+++ data_gen.rb
@@ -1,16 +1,28 @@
 def make_data
 
-	t = Time.now
+	t = Time.new(2015, 11, 7)
 	c = 0.5
 	prev = rand(1..100)
 
-	for i in (1..72)
+	for i in (0..11)
 
 		value = (c*prev + (1 - c)*rand(1..100)).round(0)
-		puts '{date: "' + (t+60*60*i).to_s + '", value: ' + value.to_s + '},'
+		puts '{time: "' + (t+60*60*i *2).to_s + '", value: ' + value.to_s + '},'
 		prev = value
 	end
 
 end
+
+make_data
+
+puts("soil")
+
+make_data
+
+puts("/////////")
+
+make_data
+
+puts("/////////")
 
 make_data
