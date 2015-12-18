c3f27cc245fbb89c088926be20f2a0fced8116d2
alex-mac <meet.alexmac@gmail.com>
2015-12-18 10:24:26 -0800

    changed onscroll

--- public/app/onScroll.js
+++ public/app/onScroll.js
@@ -2,7 +2,7 @@ angular.module('onScroll', [])
 .directive("scroll", function ($window) {
   return function(scope, element, attrs) {
     angular.element($window).bind("scroll", function() {
-      if (this.pageYOffset >= 750) {
+      if (this.pageYOffset >= 1100) {
         scope.boolChangeClass = true;
       } else {
         scope.boolChangeClass = false;
